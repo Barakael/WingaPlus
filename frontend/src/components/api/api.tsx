33bd2ffe0942ecb90e0b7ec9deeda09760e004b3
Barakael Lucas <barakaellucas2019@gmail.com>
2025-12-03 04:36:53 +0300

    chore: update BASE_URL in api.tsx to use domain name for SSL certificate
    - Changed BASE_URL from 'https://138.197.202.136' to 'https://wingapro.app' to ensure compatibility with the SSL certificate for production.

--- frontend/src/components/api/api.tsx
+++ frontend/src/components/api/api.tsx
@@ -2,5 +2,5 @@
 // export const BASE_URL = 'http://95.111.247.129';
 // export const BASE_URL = 'http://127.0.0.1:8000';
 
-// for production
-export const BASE_URL = 'https://138.197.202.136';
+// for production - use domain name to match SSL certificate
+export const BASE_URL = 'https://wingapro.app';
