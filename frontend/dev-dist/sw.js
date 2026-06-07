3e0002a6dd508209c368038286ebcc1d80e1ef6f
barakael <barakaellucas2019@gmail.com>
2026-06-07 18:49:28 +0300

    feat: add serial_number, laptop_name, and ram columns to sales table for enhanced product tracking

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.jo3cc016oeo"
+    "revision": "0.6do80jgh0a4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
